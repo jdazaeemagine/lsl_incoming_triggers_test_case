3e89a1fef43d9e8804e076582236aa5d81b892f3
jdazaeemagine <55236126+jdazaeemagine@users.noreply.github.com>
2019-12-09 18:21:23 +0100

    program done

---
+++ test.py
@@ -0,0 +1,37 @@
+"""@author: jdaza"""
+
+import pylsl as lsl
+import datetime
+import time
+import random
+import sys 
+
+
+if __name__ == "__main__":
+    
+	print("LSL INCOMING TRIGGERS TEST\n")
+	
+	name = "stream_trigger"
+
+	si = lsl.StreamInfo(name=name, type='Markers', source_id='testing_dep', channel_format=lsl.cf_string)
+	so = lsl.stream_outlet(si)
+	
+	print("It will take 5 seconds to start sending triggers...")
+	time.sleep(5) # Make sure that eego got the news of the new stram and subcribes to it. Not needed from the python side
+    
+    
+	triggers = int(sys.argv[1]) #number of triggers
+	sleeping_time = int(sys.argv[2]) #time in seconds
+
+	cont = 1
+
+	while(cont<=triggers or triggers is -1):
+	
+		string = "%i -> %s"%(cont, datetime.datetime.now().time())
+    
+		print(string)
+    
+		time.sleep(sleeping_time)
+		
+		so.push_sample([string], lsl.local_clock())
+		cont+=1
